3403290f763c4d23ca83333a63514d840490980f
MarcosLabra <mlabra.dev@gmail.com>
2022-04-20 15:55:39 -0300

    hago el menu desplegable de turismo

---
+++ js/index.js
@@ -0,0 +1,3 @@
+const turismoDropDown = document.getElementById('turismo-dropdown');
+document.getElementById('turismo-dropdown-btn').addEventListener('click', (e) => {turismoDropDown.classList.toggle('show');});
+
